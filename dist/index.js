ee89298cb6485ead899c80323ef0ac13073d6b62
Stephen <stephennguyen24@gmail.com>
2024-09-17 07:23:03 +0700

    feat: implement logic for adding csv columns and rows

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,21 @@
 "use strict";
-let a = 1;
+class CSVWriter {
+    constructor(columns) {
+        this.columns = columns;
+        this.csv = this.columns.join(",") + "\n";
+    }
+    addRows(values) {
+        let rows = values.map((value) => this.formatRow(value));
+        this.csv += rows.join("\n");
+        console.log(this.csv);
+    }
+    formatRow(payment) {
+        // Return a string of all payment values separated by a comma
+        return this.columns.map((col) => payment[col]).join(",");
+    }
+}
+const writer = new CSVWriter(["id", "amount", "to", "notes"]);
+writer.addRows([
+    { id: 1, amount: 50, to: "yoshi", notes: "for design work" },
+    { id: 2, amount: 50, to: "stephen", notes: "for dev work" },
+]);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-let a = 1;
+interface Payment {
+  id: number;
+  amount: number;
+  to: string;
+  notes: string;
+}
+
+type PaymentColumns = ("id" | "amount" | "to" | "notes")[];
+
+class CSVWriter {
+  constructor(private columns: PaymentColumns) {
+    this.csv = this.columns.join(",") + "\n";
+  }
+
+  private csv: string;
+
+  addRows(values: Payment[]): void {
+    let rows = values.map((value) => this.formatRow(value));
+
+    this.csv += rows.join("\n");
+
+    console.log(this.csv);
+  }
+
+  private formatRow(payment: Payment): string {
+    // Return a string of all payment values separated by a comma
+    return this.columns.map((col) => payment[col]).join(",");
+  }
+}
+
+const writer = new CSVWriter(["id", "amount", "to", "notes"]);
+
+writer.addRows([
+  { id: 1, amount: 50, to: "yoshi", notes: "for design work" },
+  { id: 2, amount: 50, to: "stephen", notes: "for dev work" },
+]);
